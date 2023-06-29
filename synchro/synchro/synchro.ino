96266cd73f39ba0d578c6a9ea8cfc9fe869cde8f
vwu1888 <vwu1888@gmail.com>
2023-06-28 22:57:58 -0400

    synchronizers are functional now

--- synchro/synchro/synchro.ino
+++ synchro/synchro/synchro.ino
@@ -4,41 +4,65 @@ const int outputOne = 10;
 const int outputTwo = 11;
 
 bool isReady = false;
+bool hasResponse = false;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(19200);
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(synchroOne, INPUT);
   pinMode(synchroTwo, INPUT);
   pinMode(outputOne, OUTPUT);
   pinMode(outputTwo, OUTPUT);
+  
 }
 
 void loop() {
-  if (isReady) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(outputOne, HIGH);  
-    digitalWrite(outputTwo, HIGH);  
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
-    digitalWrite(outputOne, LOW);  
-    digitalWrite(outputTwo, LOW);  
-    isReady = waitDrone();
-  }
+  String result = waitDrone();
 
-  if (isReady, digitalRead(synchroOne), digitalRead(synchroTwo)) {
-    Serial.write("SYNCED");
-    isReady = false;
+  if (result == "READY") {
+    syncUp();
+  } else if (result == "FINISH") {
+    ready();
+  } else if (result == "START") {
+    notReady();
+  } else if (result == "UNSYNC") {
+    ready();
+  } else if (result == "RESYNC") {
+    notReady();
+  } else {
+    notReady();
   }
 }
 
-bool waitDrone() {
-  while(Serial.available() == 0) {}
+bool syncUp() {
+  ready();
+
+  while (digitalRead(synchroOne) == LOW || digitalRead(synchroTwo) == LOW) {}
+
+  Serial.write("SYNCED\n");
+  delay(500);
+  notReady();
+}
+
+void ready() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(outputOne, HIGH);
+  digitalWrite(outputTwo, HIGH);
+}
+
+void notReady() {
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(outputOne, LOW);
+  digitalWrite(outputTwo, LOW);
+}
+
+String waitDrone() {
+  while (Serial.available() == 0) {}
 
   String result = Serial.readString();
   result.trim();
   Serial.flush();
 
-  return result == "READY";
+  return result;
 }
