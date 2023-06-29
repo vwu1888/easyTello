b9e4f7807d6470b2997ade4ee8d3392c84432c56
vwu1888 <vwu1888@gmail.com>
2023-06-28 23:00:07 -0400

    updated init with tellosync

--- tellosync/__init__.py
+++ tellosync/__init__.py
@@ -1,9 +1,10 @@
-__title__ = 'easytello'
-__author__ = 'Ezra Fielding'
+__title__ = 'tellosync'
+__author__ = 'Ezra Fielding, Vincent Wu'
 __liscence__ = 'MIT'
 __copyright__ = 'Copyright 2019 Ezra Fielding'
-__version__ = '0.0.8'
-__all__ = ['tello', 'stats']
+__version__ = '0.0.6'
+__all__ = ['tello', 'stats', 'synchro']
 
 from .tello import Tello
 from .stats import Stats
+from .synchro import Synchro
